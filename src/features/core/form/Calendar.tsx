d3892b6639aa3f36ffe12304c131c57dc8f4caee
Gabriel Macus <gabrielmacus@gmail.com>
2024-08-12 10:15:09 -0300

    Generating calendar days

--- src/features/core/form/Calendar.tsx
+++ src/features/core/form/Calendar.tsx
@@ -11,20 +11,47 @@ export interface CalendarProps {
     options: CalendarOptions
 }
 
+const weekDays:{[key:string]:number} = {
+    mon:0,
+    tue:1,
+    wed:2,
+    thu:3,
+    fri:4,
+    sat:5,
+    sun:6
+}
 
 export default function Calendar(props: CalendarProps) {
     const { t } = useTranslation()
-    const firstDay = props.options.firstDay ?? 'mon'
-    const weekDays: { [key: string]: string }[] = t("weekDays", { returnObjects: true })
+    const firstDay = props.options.firstDay ?? 'sat'
+    const firstDayIndex = weekDays[firstDay]
+    //const weekDays: { [key: string]: string }[] = t("weekDays", { returnObjects: true })
 
     function generateCalendarDays(month: number, year: number) {
         month-=1
         
-        const monthStart = Date.UTC(year, month + 1, 1)
-        const monthEnd = Date.UTC(year, month + 2, 0)
+        const monthStart = Date.UTC(year, month, 1)
+        const monthEnd = Date.UTC(year, month + 1, 0)
+        
 
-        const monthStartDay = Intl.DateTimeFormat('en', { weekday: 'short' }).format(monthStart).toLowerCase()
-        const monthEndDay = Intl.DateTimeFormat('en', { weekday: 'short' }).format(monthEnd).toLowerCase()
+        const monthStartDay = Intl.DateTimeFormat('en', { weekday: 'short',timeZone:"UTC" }).format(monthStart).toLowerCase()
+        const monthStartIndex = weekDays[monthStartDay]
+        const monthStartOffset = monthStartIndex - firstDayIndex - 1
+
+        let currentDay = 1 - monthStartOffset
+        while(true){
+            const date = Date.UTC(year, month, currentDay)
+            console.log(new Date(date))
+            break
+        }
+
+
+
+        //console.log("AA",monthStartDay,monthStartIndex, firstDayIndex, monthStartIndex - firstDayIndex)
+        
+        
+        
+        //const monthEndDay = Intl.DateTimeFormat('en', { weekday: 'short' }).format(monthEnd).toLowerCase()
 
 
 
